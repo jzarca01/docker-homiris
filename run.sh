23e3217a8200eb951b3f6900ca66927bf726f9d1
Jérémie Zarca <jzarca01@users.noreply.github.com>
2023-05-15 13:15:14 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --log-opt max-size=10m --log-opt max-file=5 --rm --env-file .env homiris
+docker run -d --name homiris --restart=unless-stopped --log-driver json-file --log-opt max-size=10m --log-opt max-file=5 --env-file .env homiris
